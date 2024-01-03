115001dc84b2d630c9048c8a2ec1fa3fc4138395
smellberg <smellberg@gmail.com>
2024-01-03 11:43:26 +0100

    Test

--- src/test/java/se/dsve/PersonCrudTests.java
+++ src/test/java/se/dsve/PersonCrudTests.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class PersonCrudTests {
 
@@ -14,17 +15,43 @@ public class PersonCrudTests {
     @BeforeEach
     public void setUp() {
         // TODO: Implement setup logic
+        // Konfigurera mockobjektet
+        personCrud = mock(PersonCrud.class);
     }
 
     @Test
     public void testAddPerson() {
         // TODO: Implement testAddPerson logic
+        // Konfigurera mockens beteende
+        doNothing().when(personCrud).addPerson(any(Person.class));
+
+        //Testa
+        personCrud.addPerson(new Person("TestNamn", 10));
+
+       //Om testet misslyckas
         fail("Test not yet implemented");
     }
 
     @Test
     public void testGetPersonByName() {
         // TODO: Implement testGetPersonByName logic
+        //Testperson
+        Person testPerson = new Person("TestNamn", 10);
+
+        // Konfigurera mockens beteende
+        when(personCrud.getPersonByName(eq("TestNamn"))).thenReturn(testPerson);
+
+        //Testa
+        Person result = personCrud.getPersonByName("TestNamn");
+
+        //Kolla att resultatet är sant
+        assertEquals("TestNamn", result.getName());
+        assertEquals(10, result.getAge());
+
+        // Kolla att metoden getPersonByName har anropats med rätt argument
+        verify(personCrud).getPersonByName("TestNamn");
+
+        //Om testet misslyckas
         fail("Test not yet implemented");
     }
 
